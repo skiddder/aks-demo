38be401df0cdeefda692e348ce7159c807aeaf41
Simon Schwingel <sischwin@microsoft.com>
2020-01-09 21:34:59 +0100

    init

---
+++ keyvault-secrets.sh
@@ -0,0 +1,4 @@
+az keyvault secret set --name SQL-SERVER --value sqlserverugf9246.database.windows.net --vault-name team9kv
+az keyvault secret set --name SQL-USER --value sqladminuGf9246 --vault-name team9kv
+az keyvault secret set --name SQL-PASSWORD --value 8F4030Vk3 --vault-name team9kv
+az keyvault secret set --name SQL-DBNAME --value mydrivingDB --vault-name team9kv
